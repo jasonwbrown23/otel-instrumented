df5924e20936e841a60796168e49e52885fa8bb7
Jason Brown <jason.brown@atomicfi.com>
2022-05-24 15:18:51 -0700

    Add router with fake latency

---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/jasonwbrown23/otel-instrumented
+
+go 1.18
+
+require github.com/gorilla/mux v1.8.0 // indirect
--- main.go
+++ main.go
@@ -1,9 +1,37 @@
 package main
 
 import (
-	"fmt"
+    "fmt"
+    "net/http"
+    "math/rand"
+    "time"
+
+    "github.com/gorilla/mux"
 )
 
 func main() {
   fmt.Println("Starting Simple OTEL Helloworld Application")
+  
+  rand.Seed(time.Now().UnixNano())
+  // Adding Gorilla Mux Router
+  r := mux.NewRouter()
+  r.HandleFunc("/", HomeHandler)
+
+  http.ListenAndServe(":8080", r)
+}
+
+func HomeHandler(w http.ResponseWriter, r *http.Request) {
+  FakeLatency()
+  w.WriteHeader(http.StatusOK)
+  w.Write([]byte("Hello World"))
+}
+
+func FakeLatency(){
+   w := randInt(200, 1000)
+   fmt.Printf("Faking A latency of: %d ms", w)
+   time.Sleep(time.Duration(w) * time.Millisecond)
+}
+
+func randInt(min int, max int) int {
+  return min + rand.Intn(max-min)
 }
